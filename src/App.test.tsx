dcbc67f3d7e7b398de9099146103e6af8013ffec
ferretdayo <ferretdayo@hotmail.co.jp>
2019-12-26 10:17:41 +0900

    add: lcov

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, Middleware } from 'redux'
+
 import App from './App'
+import { rootReducer } from './redux/stores'
+
+const middlewares: Middleware<any, any, any>[] = [thunk]
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div,
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
